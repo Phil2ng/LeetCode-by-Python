e9fc4eff05b67f29f36aa76d01c18e0d25896ebb
王飞 <f1570053668@163.com>
2017-11-17 19:47:36 +0800

    20171117

---
+++ 191. Number of 1 Bits.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n:
+            count += n & 1
+            n >>= 1
+        return count
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.hammingWeight(11))
