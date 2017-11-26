c8b5c41e64382c5146480dcc67863364dda47f90
王飞 <f1570053668@163.com>
2017-11-26 20:35:45 +0800

    20171126

---
+++ 219. Contains Duplicate II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] in d:
+                j = d[nums[i]]
+                if i - j <= k:
+                    return True
+            d[nums[i]] = i
+        return False
