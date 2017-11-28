7d59704f08b3cc0e6f3d9334ed352328d683b445
王飞 <f1570053668@163.com>
2017-11-28 18:40:47 +0800

    20171128

---
+++ 231. Power of Two.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n:
+            return False
+        while n != 1:
+            if n % 2:
+                return False
+            n /= 2
+        return True
