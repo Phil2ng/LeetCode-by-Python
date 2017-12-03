928fc18958fbdc69a2029a370cfc074559f3a72a
王飞 <f1570053668@163.com>
2017-12-03 10:48:08 +0800

    20171203

---
+++ 257. Binary Tree Paths.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        if not root:
+            return []
+        return [str(root.val) + '->' + path
+                for kid in (root.left, root.right) if kid
+                for path in self.binaryTreePaths(kid)] or [str(root.val)]
