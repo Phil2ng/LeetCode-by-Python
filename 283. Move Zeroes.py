f7667ac0fde88bb229222479d0c262b361b7ea90
王飞 <f1570053668@163.com>
2017-12-08 10:49:25 +0800

    20171208

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        lastNonZeroFoundAt = 0
+        for cur in range(len(nums)):
+            if nums[cur] != 0:
+                nums[lastNonZeroFoundAt], nums[cur] = nums[cur], nums[lastNonZeroFoundAt]
+                lastNonZeroFoundAt += 1
