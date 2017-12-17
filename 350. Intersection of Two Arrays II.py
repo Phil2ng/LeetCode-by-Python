dc5c6c4b77ad730f9eab84a574a102e176d3d2da
王飞 <f1570053668@163.com>
2017-12-17 09:10:10 +0800

    20171217

---
+++ 350. Intersection of Two Arrays II.py
@@ -0,0 +1,23 @@
+class Solution:
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1.sort()
+        nums2.sort()
+        res = []
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+        return res
+
+        # return list((collections.Counter(nums1) & collections.Counter(nums2)).elements())
