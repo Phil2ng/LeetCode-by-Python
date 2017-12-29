878a5e89710238976f945fe7c4cef7ccb943b169
王飞 <f1570053668@163.com>
2017-12-29 21:18:37 +0800

    20171229

---
+++ 225. Implement Stack using Queues.py
@@ -0,0 +1,31 @@
+class MyStack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        return self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.stack
