9c521cef864259c1e4a41f719d601859e822a963
王飞 <f1570053668@163.com>
2018-01-03 21:20:07 +0800

    20180103

---
+++ 414. Third Maximum Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = set(nums)
+        if len(nums) < 3:
+            return max(nums)
+        nums.remove(max(nums))
+        nums.remove(max(nums))
+        return max(nums)
